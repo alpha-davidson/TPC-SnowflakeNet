1cab3b31693e63dcc3dcb3bcee639f706bfdfbea
bewagner <bewagner@davidson.edu@alpha.ada.davidson.edu>
2025-02-12 16:31:30 -0500

    Documentation

--- data/process_mg_o_combo.py
+++ data/process_mg_o_combo.py
@@ -1,6 +1,6 @@
 """
-Processes .h5 files of simulated AT-TPC data into one
-numpy array for track completion
+Processes and cuts .h5 files of simulated AT-TPC data
+into one numpy array for track completion
 
 Author: Ben Wagner
 Date Created: 2/12/25
@@ -48,6 +48,59 @@ def process_file(file_path):
     return data, lengths
 
 
+def sample(data, lengths, n_complete, rng):
+    """
+    Samples events to a set number of total points
+
+    Parameters:
+        data: numpy.ndarray - data to sample
+        lengths: numpy.ndarray - number of unique points in each event
+        n_complete: int - number of points to sample to
+        rng: numpy.random._generator.Generator - random number generator 
+            (usually an instance of numpy.random.default_rng())
+
+    Returns:
+        sampled: numpy.ndarray - sampled events
+    """
+
+    sampled = np.ndarray((len(lengths), n_complete, 4))
+    ZERO = np.array([0.0, 0.0, 0.0])
+
+    idx = 0
+    for i, ev in enumerate(data):
+
+        if lengths[i] == 0: # Discard
+            continue
+
+        elif lengths[i] < n_complete: # Up sample
+
+            # Get complete event
+            og_l, l = lengths[i], lengths[i]
+            sampled[idx, :og_l] = ev[:og_l]
+
+            # Randomly select non zero points to upsample with
+            while l < n_complete:
+                chosen = rng.choice(ev[:og_l])
+                while np.array_equal(chosen, ZERO):
+                    chosen = rng.choice(ev[:og_l])
+                sampled[idx, l] = chosen
+                l += 1
+
+        elif lengths[i] > n_complete: # Down sample
+
+            # Randomly select n_complete points
+            whole_ev = ev[:lengths[i]]
+            rng.shuffle(whole_ev, axis=0)
+            sampled[idx] = whole_ev[:n_complete]
+
+        else: # No sampling necessary
+            sampled[idx] = ev[:n_complete]
+        
+        idx += 1
+
+    return sampled
+
+
 def filter_invalid(data, lengths, n_complete, min_n_unique):
     """
     Filters events to a minimum number of unique points
@@ -55,7 +108,12 @@ def filter_invalid(data, lengths, n_complete, min_n_unique):
     Parameters:
         data: numpy.ndarray - events to be filtered
         lengths: numpy.ndarray - number of unique points per event
-        n_complete: int - 
+        n_complete: int - number of points in a complete event
+        min_n_unique: int - mininum number of unique points in an event
+
+    Returns:
+        filtered: numpy.ndarray - filtered data
+        filtered_lengths: numpy.ndarray - number of unique points in each event
     """
 
     filtered = np.zeros((np.count_nonzero(np.where(lengths < min_n_unique, 0, lengths)), n_complete, 4))
@@ -71,54 +129,19 @@ def filter_invalid(data, lengths, n_complete, min_n_unique):
         filtered_lengths[idx] = n_unique
         idx += 1
 
-    print("Filtration Complete")
-    print(f"\tMinimum Length: {np.amin(filtered_lengths)}")
-    print(f"\tMaximum Length: {np.amax(filtered_lengths)}")
-
     return filtered, filtered_lengths
-
-
-def sample(data, lengths, n_complete, rng):
-    """
-    Samples events to 512 total points
-    """
-
-    sampled = np.ndarray((len(lengths), n_complete, 4))
-    ZERO = np.array([0.0, 0.0, 0.0])
-
-    idx = 0
-    for i, ev in enumerate(data):
-
-        if lengths[i] == 0:
-            # Discard
-            continue
-        elif lengths[i] < n_complete:
-            # Up sample
-            og_l, l = lengths[i], lengths[i]
-            sampled[idx, :og_l] = ev[:og_l]
-            while l < n_complete:
-                chosen = rng.choice(ev[:og_l])
-                while np.array_equal(chosen, ZERO):
-                    chosen = rng.choice(ev[:og_l])
-                sampled[idx, l] = chosen
-                l += 1
-        elif lengths[i] > n_complete:
-            # Down sample
-            whole_ev = ev[:lengths[i]]
-            rng.shuffle(whole_ev, axis=0)
-            sampled[idx] = whole_ev[:n_complete]
-        else:
-            # No sampling necessary
-            sampled[idx] = ev[:n_complete]
-        
-        idx += 1
-
-    return sampled
 
 
 def scale_data(data, ranges):
     """
     Min/Max scales data based on ranges (logs qs first)
+
+    Parameters:
+        data: numpy.ndarray - data to scale
+        ranges: dict - contains min and max values of x,y,z, and ln(q)
+
+    Returns:
+        scaled: numpy.ndarray - scaled data
     """
 
     scaled = np.ndarray(data.shape)
@@ -171,31 +194,16 @@ if __name__ == '__main__':
     RNG = np.random.default_rng()
 
     # Process
-    print("Beginning processing ...")
     mg_data, mg_lengths = process_file(SIM_MG_FILE_PATH)
-    print("22Mg done processing ...")
-    print(f"\tData shape: {mg_data.shape}\n\tLengths shape: {mg_lengths.shape}")
     o_data, o_lengths = process_file(SIM_O_FILE_PATH)
-    print("16O done processing ...")
-    print(f"\tData shape: {o_data.shape}\n\tLengths shape: {o_lengths.shape}")
 
     # Sample
-    print("Beginning sampling ...")
     mg_sampled = sample(mg_data, mg_lengths, N_COMPLETE, RNG)
-    print("22Mg done sampling ...")
-    print(f"\tData shape: {mg_sampled.shape}")
     o_sampled = sample(o_data, o_lengths, N_COMPLETE, RNG)
-    print("16O done sampling ...")
-    print(f"\tData shape: {o_sampled.shape}")
 
     # Filter
-    print("Beginning filtering ...")
     mg_filtered, mg_filt_lens = filter_invalid(mg_sampled, mg_lengths, N_COMPLETE, MIN_N_UNIQUE)
-    print("22Mg done filtering ...")
-    print(f"\tData shape: {mg_filtered.shape}\n\tLengths shape: {mg_filt_lens.shape}")
     o_filtered, o_filt_lens = filter_invalid(o_sampled, o_lengths, N_COMPLETE, MIN_N_UNIQUE)
-    print("16O done filtering ...")
-    print(f"\tData shape: {o_filtered.shape}\n\tLengths shape: {o_filt_lens.shape}")
 
     # Combine datasets
     labels = np.vstack((mg_filtered, o_filtered))
@@ -205,35 +213,25 @@ if __name__ == '__main__':
     random_cut = np.ndarray((len(labels), N_PARTIAL, 4))
     random_samp = np.ndarray((len(labels), N_PARTIAL, 4))
 
-    print("Beginning cutting ...")
     for i, ev in enumerate(labels):
         center[i] = center_cut(ev, (0,0), N_COMPLETE-N_PARTIAL)
         random_cut[i] = rand_cut(ev, N_COMPLETE-N_PARTIAL, RNG)
         sampled = ev
         RNG.shuffle(sampled)
         random_samp[i] = sampled[:N_PARTIAL]
-    print("Done cutting ...")
 
-    # Scale and Split into train, val, test sets (60, 20, 20)
+    # Combine and scale
     labels = np.vstack((labels, labels, labels))
     feats = np.vstack((center, random_cut, random_samp))
 
-    print("Beginning scaling ...")
-    print(np.min(labels[:, :, -1]), np.max(labels[:, :, -1]))
-    print(np.min(feats[:, :, -1]), np.max(feats[:, :, -1]))
     labels = scale_data(labels, RANGES)
     feats = scale_data(feats, RANGES)
-    assert np.max(labels) <= 1 and np.min(labels) >= 0, f"Problem in scaling labels: min = {np.min(labels)}\t max = {np.max(labels)}"
-    assert np.max(feats) <= 1 and np.min(feats) >= 0, f"Problem in scaling features: min = {np.min(feats)}\t max = {np.max(feats)}"
-    print("Done scaling ...")
 
-    print("Beginning splitting ...")
+    # Split into train, val, test sets (60, 20, 20)
     train_feats, val_feats, train_labels, val_labels = train_test_split(feats, labels, test_size=0.2)
     train_feats, test_feats, train_labels, test_labels = train_test_split(train_feats, train_labels, test_size=0.25)
-    print("Done splitting ...")
 
     # Save datasets
-    print("Beginning saving ...")
     np.save(TRAIN_FEATS_SAVE_PATH, train_feats)
     np.save(VAL_FEATS_SAVE_PATH, val_feats)
     np.save(TEST_FEATS_SAVE_PATH, test_feats)
@@ -241,14 +239,4 @@ if __name__ == '__main__':
     np.save(TRAIN_LABELS_SAVE_PATH, train_labels)
     np.save(VAL_LABELS_SAVE_PATH, val_labels)
     np.save(TEST_LABELS_SAVE_PATH, test_labels)
-    print("Done saving ...")
-
-    print("Program sucessfully finished!")
-    print("train_feats :", train_feats.shape)
-    print("val_feats :", val_feats.shape)
-    print("test_feats :", test_feats.shape)
-    print()
-    print("train_labels :", train_labels.shape)
-    print("val_labels :", val_labels.shape)
-    print("test_labels :", test_labels.shape)
 
